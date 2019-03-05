9bf4bb522a899d237985218f7b69fdfff0cbdb50
nico <paquet.nicolas@gmail.com>
2019-03-05 23:14:52 +0100

    Màj des fixtures user et articles

--- src/DataFixtures/ArticlesFixtures.php
+++ src/DataFixtures/ArticlesFixtures.php
@@ -27,7 +27,7 @@ class ArticlesFixtures extends Fixture
                       ->setDescription($faker->paragraph());
             $manager -> persist($category);
 
-            //créer entre 4 et 6 Articles
+            //créer entre 6 et 10 Articles
             for ($j=1; $j<=mt_rand(4,6);$j++){
                 $article=new Article();
 
@@ -42,63 +42,27 @@ class ArticlesFixtures extends Fixture
                 $manager-> persist($article);
             }
         }
-
-
-
-
-        // // créer 3 catégories fakées
-        // for ($i=1; $i<=3; $i++){
-        //     $category = new Category();
-        //     $category ->setTitle($faker->sentence())
-        //               ->setDescription($faker->paragraph());
-        //     $manager -> persist($category);
-        //
-        //     //créer entre 4 et 6 Articles
-        //     for ($j=1; $j<=mt_rand(4,6);$j++){
-        //         $article=new Article();
-        //
-        //         $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>';
-        //
-        //         $article->setTitle($faker->sentence())
-        //                 -> setContent($content)
-        //                 -> setImage($faker->imageUrl())
-        //                 -> setCreatedAt($faker->dateTimeBetween('-6 months'))
-        //                 -> setCategory($category);
-        //
-        //         $manager-> persist($article);
-        //
-        //         // commentaires
-        //         $content = '<p>' . join($faker->paragraphs(2), '</p><p>') . '</p>';
-        //         $now = new \DateTime();
-        //         $interval = $now->diff($article->getCreatedAt());
-        //         $days = $interval->days;
-        //         $minimum = '-' . $days . ' days';
-        //
-        //         for ($k = 1; $k<=mt_rand(4,10); $k++){
-        //             $comment=new Comment();
-        //             $comment-> setAuthor($faker->name)
-        //                     -> setContent($content)
-        //                     -> setCreatedAt($faker->dateTimeBetween($minimum))
-        //                     -> setArticle($article);
-        //             $manager-> persist($comment);
-        //         }
-        //     }
-        // }
-
-        // $product = new Product();
-        // $manager->persist($product);
-        /*
-        for ($i=1; $i<=10;$i++){
-            $article=new Article();
-            $article->setTitle("Titre de l'article n°$i")
-                    -> setContent("<p>Contenu de l'article n°$i</p>")
-                    -> setImage("http://placehold.it/350*150")
-                    -> setCreatedAt(new \DateTime());
-
-            $manager-> persist($article);
-        }
-        */
-
         $manager->flush();
     }
 }
+
+
+
+
+/**
+// entre 1 et 4 commentaires sur cet article
+$content = '<p>' . join($faker->paragraphs(2), '</p><p>') . '</p>';
+$now = new \DateTime();
+$interval = $now->diff($article->getCreatedAt());
+$days = $interval->days;
+$minimum = '-' . $days . ' days';
+
+for ($k = 1; $k<=mt_rand(1,4); $k++){
+$comment=new Comment();
+$comment-> setContent($content)
+-> setCreatedAt($faker->dateTimeBetween($minimum))
+-> setArticle($article)
+-> setUser($user);
+$manager-> persist($comment);
+}
+*/
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,6 +4,8 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 use App\Entity\Phone;
 use App\Entity\Article;
@@ -13,10 +15,25 @@ use App\Entity\Comment;
 use App\Entity\User;
 use App\Entity\Adress;
 use App\Entity\Country;
+use App\DataFixtures\AppFixtures;
 Use App\Entity\City;
 
 class UserFixtures extends Fixture
 {
+    /**
+    Nécessaire pour utiliser encodePassword dans la fixture
+    */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+    Fixtures
+    */
+
     public function load(ObjectManager $manager)
     {
         $faker = \Faker\Factory::create('fr_FR');
@@ -27,80 +44,44 @@ class UserFixtures extends Fixture
 
         $city = new City();
 
-        // créer 5 utilisateurs fakés
-        for ($k=1; $k<=5; $k++){
+        // créer 25 utilisateurs fakés
+        for ($k=1; $k<=25; $k++){
 
             $user = new User();
 
             $userAdress = new Adress();
-            $userAdress->setType("test")
-                       ->setNum("123")
-                       ->setStreetName("avenue du test")
-                       ->setPostBox("B12")
+            $userAdress->setType("Domicile")
+                       ->setNum($faker->buildingNumber())
+                       ->setStreetName($faker->streetName())
+                       ->setPostBox($faker->randomDigitNotNull())
                        ->addUser($user);
             $manager->persist($userAdress);
 
             $phoneType='Domicile';
             $userPhone=new Phone();
             $userPhone->setType($phoneType)
-                      ->setNum("123456")
+                      ->setNum($faker->phoneNumber())
                       ->addUser($user);
             $manager->persist($userPhone);
 
-
-
-
-            $city->setCityName("Mons")
-                 ->setZip("3000")
+            $city->setCityName($faker->city())
+                 ->setZip($faker->postcode())
                  ->addAdress($userAdress)
                  ->setCountry($country);
             $manager->persist($city);
 
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,'testtest'));
+
             $user->setEmail($faker->email)
                  ->setUsername($faker->userName)
-                 ->setPassword("testtest")
-                 ->setName("Pqt")
-                 ->setFirstname("Nico")
-                 ->setBirthday($faker->dateTimeBetween('-6 years'))
+                 ->setName($faker->lastName())
+                 ->setFirstname($faker->firstName())
+                 ->setBirthday($faker->dateTimeBetween('-77 years', '-6years'))
                  ->setCreatedUser($faker->dateTimeBetween('-6 months'))
-                 ->setBelt("noir")
-                 ->setCreatedUser($faker->dateTimeBetween('-3 months'));
+                 ->setBelt($faker->safeColorName());
             $manager-> persist($user);
 
-            // créer 3 catégories fakées
-            for ($i=1; $i<=3; $i++){
-                $category = new Category();
-                $category ->setTitle($faker->sentence())
-                          ->setDescription($faker->paragraph());
-                $manager -> persist($category);
-
-                //créer 1 Article
-                    $article=new Article();
-
-                    $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>';
-                    $article->setTitle($faker->sentence())
-                            -> setContent($content)
-                            -> setImage($faker->imageUrl())
-                            -> setCreatedAt($faker->dateTimeBetween('-6 months'))
-                            -> setCategory($category);
-                    $manager-> persist($article);
-
-                    // entre 1 et 4 commentaires sur cet aric
-                    $content = '<p>' . join($faker->paragraphs(2), '</p><p>') . '</p>';
-                    $now = new \DateTime();
-                    $interval = $now->diff($article->getCreatedAt());
-                    $days = $interval->days;
-                    $minimum = '-' . $days . ' days';
-
-                    for ($k = 1; $k<=mt_rand(1,4); $k++){
-                        $comment=new Comment();
-                        $comment-> setContent($content)
-                                -> setCreatedAt($faker->dateTimeBetween($minimum))
-                                -> setArticle($article)
-                                -> setUser($user);
-                        $manager-> persist($comment);
-                    }
-                }
             $manager->flush();
         }
     }
