38cae0a9fc98f4b99e756828583a5eb4cc2e12d9
nico <paquet.nicolas@gmail.com>
2019-03-06 16:21:00 +0100

    Modif temporaire
    Attribut name et firstName peuvent être nuls pour la phase det développement

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -60,12 +60,12 @@ class User implements UserInterface
     private $adress;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $name;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $firstname;
 
@@ -75,7 +75,7 @@ class User implements UserInterface
     private $birthday;
 
     /**
-     * @ORM\Column(type="date")
+     * @ORM\Column(type="date", nullable=true)
      */
     private $createdUser;
 
