d8b0e841ee70ae00c45a0c1edd4a2aed9680f3ca
aLissoir <anthonylissoir@hotmail.com>
2019-03-14 18:55:19 +0100

    Essaie

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -19,7 +19,7 @@ use Doctrine\Common\Persistence\ObjectManager;
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog")
+     * @Route("/blog", name="blogg")
      */
 
     public function index(ArticleRepository $repo)
@@ -27,7 +27,7 @@ class BlogController extends AbstractController
         $articles = $repo -> findAll();
 
 
-        return $this->render('blog/index.html.twig', [
+        return $this->render('blog/indexx.html.twig', [
             'controller_name' => 'BlogController',
             'articles' => $articles,
         ]);
