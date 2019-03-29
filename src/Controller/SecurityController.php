e71af3964d6b54c8435db12a05a0efb0632df432
Pierre <vanrijmenantp@gmail.com>
2019-03-29 22:03:19 +0100

    Mot de passe oublié

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -53,6 +53,70 @@ class SecurityController extends AbstractController
 
     }
 
+    /**
+     * @Route("/forgotten_password", name="app_forgotten_password")
+     */
+    public function forgottenPassword(
+        Request $request,
+        \Swift_Mailer $mailer,
+        TokenGeneratorInterface $tokenGenerator
+    ): Response
+    {
+        if ($request->isMethod('POST')) {
+            $email = $request->request->get('email');
+            $entityManager = $this->getDoctrine()->getManager();
+            $user = $entityManager->getRepository(User::class)->findOneByEmail($email);
+            /* @var $user User */
+            if ($user === null) {
+                $this->addFlash('danger', 'Email Inconnu');
+                return $this->redirectToRoute('homepage');
+            }
+            $token = $tokenGenerator->generateToken();
+            try{
+                $user->setResetToken($token);
+                $entityManager->flush();
+            } catch (\Exception $e) {
+                $this->addFlash('warning', $e->getMessage());
+                return $this->redirectToRoute('homepage');
+            }
+            $url = $this->generateUrl('app_reset_password', array('token' => $token), UrlGeneratorInterface::ABSOLUTE_URL);
+            $message = (new \Swift_Message('Forgot Password'))
+                ->setFrom('g.ponty@dev-web.io')
+                ->setTo($user->getEmail())
+                ->setBody(
+                    "blablabla voici le token pour reseter votre mot de passe : " . $url,
+                    'text/html'
+                );
+            $mailer->send($message);
+            $this->addFlash('notice', 'Mail envoyé');
+            return $this->redirectToRoute('homepage');
+        }
+        return $this->render('security/forgotten_password.html.twig');
+    }
+
+    /**
+     * @Route("/reset_password/{token}", name="app_reset_password")
+     */
+    public function resetPassword(Request $request, string $token, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        if ($request->isMethod('POST')) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $user = $entityManager->getRepository(User::class)->findOneByResetToken($token);
+            /* @var $user User */
+            if ($user === null) {
+                $this->addFlash('danger', 'Token Inconnu');
+                return $this->redirectToRoute('homepage');
+            }
+            $user->setResetToken(null);
+            $user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('password')));
+            $entityManager->flush();
+            $this->addFlash('notice', 'Mot de passe mis à jour');
+            return $this->redirectToRoute('homepage');
+        }else {
+            return $this->render('security/reset_password.html.twig', ['token' => $token]);
+        }
+    }
+
 }
 
     // /**
