8bc9103ec73568b81042a9b0bec5ce0dfbca58e1
aLissoir <anthonylissoir@hotmail.com>
2019-03-31 15:55:59 +0200

    Création formulaire Galeriephoto

--- src/Controller/GaleriephotoController.php
+++ src/Controller/GaleriephotoController.php
@@ -4,6 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Media;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\GaleriephotoType;
 
 class GaleriephotoController extends AbstractController
 {
@@ -12,16 +16,34 @@ class GaleriephotoController extends AbstractController
      */
     public function index()
     {
-        return $this->render('galeriephoto/index.html.twig', [
-            'controller_name' => 'GaleriephotoController',
-        ]);
+        return $this->render('galeriephoto/index.html.twig');
     }
 
     /**
      * @Route("/galeriephoto-new", name="galeriephoto_create")
-     */
-    public function create()
-    {
-        return $this->render('galeriephoto/create.html.twig');
-    }  
+     */ 
+    public function form(Media $galerie=null, Request $request, ObjectManager $manager){
+
+        if (!$galerie){
+            $galerie=New Media();
+        }
+
+        $form = $this->createForm(GaleriephotoType::class, $galerie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            if (!$galerie->getId()){
+                $galerie->setCreatedEv(new \DateTime());
+            }
+            $manager->persist($galerie);
+            $manager->flush();
+            /* return $this->redirectToRoute('blog_show',['id'=>$galerie->getId()]); */
+            return $this->redirectToRoute('galeriephoto');
+        }
+
+        return $this->render('galeriephoto/create.html.twig', [
+            'formGaleriephoto'=>$form->createView(),
+            'editMode'=> $galerie->getId()!==null
+        ]);
+    }
 }
