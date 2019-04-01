607fe294699d1921804a79e7b00cbd2c0508a752
aLissoir <anthonylissoir@hotmail.com>
2019-04-01 18:07:42 +0200

    Création de la form Encadrement

---
+++ src/Form/EncadrementType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Encadrement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EncadrementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('imagename')
+            ->add('datecreat')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Encadrement::class,
+        ]);
+    }
+}
