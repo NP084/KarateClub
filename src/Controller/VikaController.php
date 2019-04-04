2bd693a77a2d91033a917f75907a1e88c938e07c
182844 <182844@umons.ac.be>
2019-04-04 20:17:13 +0200

    MemberAdmin Controls

--- src/Controller/VikaController.php
+++ src/Controller/VikaController.php
@@ -3,13 +3,40 @@
 namespace App\Controller;
 
 use App\Entity\ContentPage;
+use App\Form\ArticleType;
+use App\Form\ContentType;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class VikaController extends AbstractController
 {
     /**
-     * @Route("/vika-{path}", name="VikaContent", requirements={"id"="\d+"})
+     * @Route("/vika-{path}-edit", name="VikaContentEdit")
+     *
+     */
+    public function vikacreate(ContentPage $content, Request $request, ObjectManager $manager ){
+
+        $form = $this->createForm(ContentType::class, $content);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $manager->persist($content);
+            $manager->flush();
+            return $this->redirectToRoute('VikeContentEdit',['id'=>$content->getId()]);
+        }
+
+        return $this->render('vika/vikacreate.html.twig', [
+            'formContent'=>$form->createView(),
+
+        ]);
+    }
+
+    /**
+     * @Route("/vika-{path}", name="VikaContent")
+     *
      */
     public function vikashow(ContentPage $contentPage){
 
@@ -17,4 +44,6 @@ class VikaController extends AbstractController
             'contentPage' => $contentPage,
         ]);
     }
+
+
 }
