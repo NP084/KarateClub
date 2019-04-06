255c7fdad0dd6708bc730a3c6e012f2e9418e70a
182844 <182844@umons.ac.be>
2019-04-06 12:22:37 +0200

    Revert "toDelete"
    This reverts commit 6626c92ea647249d56966df9fd3839e6ae8a0d5f.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -31,9 +31,7 @@ class SecurityController extends AbstractController
 
             $manager->persist($user);
             $manager->flush($user);
-            return $this->render('security/registered.html.twig');
-
-//            return $this->redirectToRoute('security_login');
+            return $this->redirectToRoute('security_login');
         }
 
             return $this->render('security/registration.html.twig', [
@@ -49,13 +47,7 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig');
 
     }
-    /**
-     * @Route("/registered", name="registered")
-     */
-    public function registered(){
-        return $this->render('security/registered.html.twig');
 
-    }
 
     /**
     * @Route("/deconnexion", name="security_logout")
