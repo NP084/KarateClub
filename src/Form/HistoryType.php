0dfc4ff981702124060ce62faaf180f70ea8222b
nico <paquet.nicolas@gmail.com>
2019-04-07 13:21:46 +0200

    mise en page de edit profil

--- src/Form/HistoryType.php
+++ src/Form/HistoryType.php
@@ -13,6 +13,10 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class HistoryType extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
