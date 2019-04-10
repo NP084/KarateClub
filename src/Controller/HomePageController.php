328088dd970a4b1ea06d94ece18becec6010be93
182844 <182844@umons.ac.be>
2019-04-10 22:01:07 +0200

    Revert "todelete"
    This reverts commit b54dc5242b05f3eafcc2f656f1474d35d1cb1364.

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,11 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\ContentPage;
-use App\Form\ContentType;
-use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomePageController extends AbstractController
@@ -14,33 +10,9 @@ class HomePageController extends AbstractController
     /**
      * @Route("/homepage", name="home_page")
      */
-    public function index($contentPage)
+    public function index()
     {
 //        return $this->render('home_page/showContent.html.twig');
-
-        return $this->render('home_page/index.html.twig', [
-            'contentPage' => $contentPage,
-        ]);
+        return $this->render('home_page/index.html.twig');
     }
-
-/**
- * @Route("/homepage-edit", name="HomeEdit")
- *
- */
-public function informationcreate(ContentPage $content, Request $request, ObjectManager $manager ){
-
-    $form = $this->createForm(ContentType::class, $content);
-    $form->handleRequest($request);
-
-    if ($form->isSubmitted() && $form->isValid()){
-
-        $manager->persist($content);
-        $manager->flush();
-        return $this->redirectToRoute('home_page',['path'=>$content->getPath()]);
-    }
-
-    return $this->render('home_page/index.html.twig', [
-        'formContent'=>$form->createView(),
-
-    ]);
-}}
+}
