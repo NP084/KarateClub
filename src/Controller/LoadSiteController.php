eced52e7134092987dd055efca5b2f4a5f42b671
nico <paquet.nicolas@gmail.com>
2019-04-14 16:48:30 +0200

    format date et liste des grades

--- src/Controller/LoadSiteController.php
+++ src/Controller/LoadSiteController.php
@@ -21,6 +21,64 @@ class LoadSiteController extends AbstractController
         $category -> setTitle('Passage de grade')
                   -> setDescription('Historique');
         $entityManager->persist($category);
+
+        $category = new Category();
+        $category -> setTitle('Participation à un stage')
+            -> setDescription('Historique');
+        $entityManager->persist($category);
+
+        $category = new Category();
+        $category -> setTitle('Participation à une compétition')
+            -> setDescription('Historique');
+        $entityManager->persist($category);
+
+        $category = new Category();
+        $category -> setTitle('Autre')
+            -> setDescription('Historique');
+        $entityManager->persist($category);
+
+        // création des CATEGORIES pour la table VIKAEVENT
+        $category = new Category();
+        $category -> setTitle('Stage')
+            -> setDescription('Event');
+        $entityManager->persist($category);
+
+        $category = new Category();
+        $category -> setTitle('Compétition')
+            -> setDescription('Event');
+        $entityManager->persist($category);
+
+        $category = new Category();
+        $category -> setTitle('Passage de grade')
+            -> setDescription('Event');
+        $entityManager->persist($category);
+
+        $category = new Category();
+        $category -> setTitle('Autre')
+            -> setDescription('Event');
+        $entityManager->persist($category);
+
+        // création des CATEGORIES pour la table ARTICLE
+        $category = new Category();
+        $category -> setTitle('Information générale')
+            -> setDescription('Articles');
+        $entityManager->persist($category);
+
+        $category = new Category();
+        $category -> setTitle('Annonce')
+            -> setDescription('Articles');
+        $entityManager->persist($category);
+
+        $category = new Category();
+        $category -> setTitle('Passage de grade')
+            -> setDescription('Articles');
+        $entityManager->persist($category);
+
+        $category = new Category();
+        $category -> setTitle('Autre')
+            -> setDescription('Articles');
+        $entityManager->persist($category);
+
         $entityManager->flush();
         return $this->redirectToRoute('home');
     }
