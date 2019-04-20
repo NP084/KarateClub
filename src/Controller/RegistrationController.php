cbc4784418416d939e8648cd2490ab55cc7ca975
aLissoir <anthonylissoir@hotmail.com>
2019-04-20 11:00:32 +0200

    comit

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,6 +4,16 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\UserConnected;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
 
 class RegistrationController extends AbstractController
 {
@@ -16,4 +26,21 @@ class RegistrationController extends AbstractController
             'controller_name' => 'RegistrationController',
         ]);
     }
+
+/**
+     * MEMBRES DE LA FAMILLE D'UN UTILISATEUR DU SITE
+     * @Route("/registration-member-family-{id}", name="registration_view_family", requirements={"idCL"="\d+"})
+     * @Route("/registration-admin-family-{id}", name="registration_admin_family", requirements={"idCL"="\d+"})
+     * @Security("has_role('ROLE_ADMIN') or user.getId() == userConnected.getId()")
+     */
+    public function indexFamily(UserConnected $userConnected)
+    {
+        $users = $userConnected->getUsers();
+        return $this->render('registration/showFamily.html.twig', [
+            'controller_name' => 'Vue des membres de sa famille',
+            'users' => $users,
+            'userConnected' => $userConnected,
+        ]);
+    }
+
 }
