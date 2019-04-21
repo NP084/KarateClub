37c91a014c1c3b4ae7f790a07df927c7aa2eaafc
182844 <182844@umons.ac.be>
2019-04-21 09:50:16 +0200

    Ongoing Registration Process

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\UserConnected;
@@ -10,6 +15,7 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
@@ -27,7 +33,48 @@ class RegistrationController extends AbstractController
         ]);
     }
 
-/**
+    /**
+     * @Route("/new_registration", name="new_registration")
+     */
+    public function new_registration(Request $request, ObjectManager $manager){
+        $user = new User();
+        $user->setName('Write a blog post');
+        $user->setFirstName('tomorrow');
+
+        $form = $this->createFormBuilder($user)
+            ->add('name', TextType::class)
+            ->add('firstname', TextType::class)
+            ->add('birthdate', DateType::class)
+            ->add('sex', TextType::class)
+            ->add('belt', TextType::class)
+            ->add('receiptdate', DateType::class)
+            ->getForm();
+
+        return $this->render('registration/registration.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+//        $form = $this->createForm(UserType::class, $user);
+//        $form->handleRequest($request);
+//
+//        if($form->isSubmitted() && $form->isValid()){
+//
+//            $manager->persist($user);
+//            $manager->flush($user);
+//            return $this->redirectToRoute('validation');
+//        }
+//
+//        return $this->render('registration/registration.html.twig', [
+//            'form' => $form->createView()
+//        ]);
+//
+//    }
+
+
+
+    /**
      * MEMBRES DE LA FAMILLE D'UN UTILISATEUR DU SITE
      * @Route("/registration-member-family-{id}", name="registration_view_family", requirements={"idCL"="\d+"})
      * @Route("/registration-admin-family-{id}", name="registration_admin_family", requirements={"idCL"="\d+"})
