6286ed1472ad6c773a4dc2ed8b7e6ffcf7b826e4
182844 <182844@umons.ac.be>
2019-04-29 16:39:31 +0200

    Label Changed + Stepper

--- src/Form/PreregistrationType.php
+++ src/Form/PreregistrationType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Registration;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,10 +15,11 @@ class PreregistrationType extends AbstractType
     {
         $builder
 
-            ->add('remark')
-            ->add('medical_care')
-            ->add('image_diffusion')
-            ->add('condition_registration')
+
+            ->add('medical_care',CheckboxType::class,['label'=> 'Prise en charge médicale'])
+            ->add('image_diffusion',CheckboxType::class,['label'=> 'Droit à l\' image'])
+            ->add('condition_registration',CheckboxType::class,['label'=> 'Conditions générales'])
+            ->add('remark',TextareaType::class,['label'=> 'Remarque éventuelle'])
 //            ->add('profil_tarif')
         ;
     }
