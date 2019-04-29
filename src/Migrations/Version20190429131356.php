fa67fe2956b13058307febd44fa413870d04adf3
182844 <182844@umons.ac.be>
2019-04-29 15:26:35 +0200

    Migration

--- src/Migrations/Version20190429131356.php
+++ src/Migrations/Version20190429131356.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190428222010 extends AbstractMigration
+final class Version20190429131356 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,9 +22,7 @@ final class Version20190428222010 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE registration DROP FOREIGN KEY FK_62A8A7A7C903FE1D');
-        $this->addSql('DROP INDEX UNIQ_62A8A7A7C903FE1D ON registration');
-        $this->addSql('ALTER TABLE registration DROP userconnected_id_id');
+        $this->addSql('ALTER TABLE vika_event ADD inscription TINYINT(1) DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -32,8 +30,6 @@ final class Version20190428222010 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE registration ADD userconnected_id_id INT NOT NULL');
-        $this->addSql('ALTER TABLE registration ADD CONSTRAINT FK_62A8A7A7C903FE1D FOREIGN KEY (userconnected_id_id) REFERENCES userConnected (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_62A8A7A7C903FE1D ON registration (userconnected_id_id)');
+        $this->addSql('ALTER TABLE vika_event DROP inscription');
     }
 }
