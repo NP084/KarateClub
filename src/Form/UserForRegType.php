decd7c754f40ff2e1a72958574ed3cee6bc5ca66
nico <paquet.nicolas@gmail.com>
2019-05-02 13:32:23 +0200

    champ obligatoire à l'inscription

--- src/Form/UserForRegType.php
+++ src/Form/UserForRegType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,8 +14,12 @@ class UserForRegType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('firstname')
+            ->add('name', null, [
+                'required'=> true,
+            ])
+            ->add('firstname', null, [
+                'required'=> true,
+            ])
             ->add('birthdate', DateType::class, [
                 'years' => range(1900, date('Y')-14),
                 'format' => 'dd-MM-yyyy',
