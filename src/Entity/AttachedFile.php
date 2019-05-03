9c74db26be6367686db26b99ffe7d0a6b11c40dd
nico <paquet.nicolas@gmail.com>
2019-05-03 13:39:31 +0200

    assert PDF

--- src/Entity/AttachedFile.php
+++ src/Entity/AttachedFile.php
@@ -50,6 +50,12 @@ class AttachedFile
      * NOTE: This is not a mapped field of entity metadata, just a simple property.
      * @Vich\UploadableField(mapping="document_picture", fileNameProperty="docname")
      * @var File
+     * @Assert\File(
+     *     maxSize="5242880",
+     *     mimeTypes = {
+     *     "application/pdf",
+     *     })
+
      */
     private $docFile;
 
