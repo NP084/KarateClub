bd22f0b591a63162f6a38aee4d7f3319187df69e
182844 <182844@umons.ac.be>
2019-05-05 10:38:45 +0200

    Revert "to delete"
    This reverts commit a67f4b5c9da9a4d0ac0ab990dfb6e379282de810.

--- src/Entity/Registration.php
+++ src/Entity/Registration.php
@@ -87,11 +87,6 @@ class Registration
      */
     private $isValidated;
 
-    /**
-     * @ORM\Column(type="blob", nullable=true)
-     */
-    private $Documents;
-
 
 
     public function __construct()
@@ -298,18 +293,6 @@ class Registration
     public function setIsValidated(?bool $isValidated): self
     {
         $this->isValidated = $isValidated;
-
-        return $this;
-    }
-
-    public function getDocuments()
-    {
-        return $this->Documents;
-    }
-
-    public function setDocuments($Documents): self
-    {
-        $this->Documents = $Documents;
 
         return $this;
     }
