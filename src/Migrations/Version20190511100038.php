2189f751503b1545a30a706d4a2534dd1ea58e1e
182844 <182844@umons.ac.be>
2019-05-11 13:10:24 +0200

    Ajout du bouton Conditions Générales
    AJouter dans gestion des événement

--- src/Migrations/Version20190511100038.php
+++ src/Migrations/Version20190511100038.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190510202358 extends AbstractMigration
+final class Version20190511100038 extends AbstractMigration
 {
     public function getDescription() : string
     {
