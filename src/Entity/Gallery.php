f485ab442c256e3ad5faa5f30572b78d4225803c
nico <paquet.nicolas@gmail.com>
2019-05-22 00:01:37 +0200

    modif suite à test sur event

--- src/Entity/Gallery.php
+++ src/Entity/Gallery.php
@@ -6,7 +6,6 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Symfony\Component\HttpFoundation\File\File;
 
